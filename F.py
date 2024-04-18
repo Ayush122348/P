91c95d5ad7eb33ef66ff5dc2f57ca65c7528a94b
Ayush122348 <122199164+Ayush122348@users.noreply.github.com>
2024-04-18 22:33:08 +0530

    Update F.py

--- F.py
+++ F.py
@@ -1,2 +1,10 @@
-while 1 == 1:
-  print("Hello!")
+from tkinter import filedialog
+from tkinter import Tk
+from tkinter import *
+root=Tk()
+root.filename=filedialog.askopenfilename(filetypes=(("Text Files",".txt")),("All Files","*.*")))
+text1=open(root.fileName).read()
+T = Text(root, height=25, width=80)
+T.pack()
+T.insert(END,text1)
+root.mainloop()
