11d630b5aecd2777f28cddb7e65dba2a26df02ec
Ayush122348 <122199164+Ayush122348@users.noreply.github.com>
2024-04-18 22:34:13 +0530

    Update F.py

--- F.py
+++ F.py
@@ -2,7 +2,7 @@ from tkinter import filedialog
 from tkinter import Tk
 from tkinter import *
 root=Tk()
-root.filename=filedialog.askopenfilename(filetypes=(("Text Files",".txt")),("All Files","*.*")))
+root.filename=filedialog.askopenfilename(filetypes=(("Text Files",".txt"),("All Files","*.*")))
 text1=open(root.fileName).read()
 T = Text(root, height=25, width=80)
 T.pack()
